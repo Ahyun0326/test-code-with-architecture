c79c6da6b85d77bba7b0598969d3da9838c54d0b
Yeseo Kim <chinkl850@gmail.com>
2024-07-27 20:43:41 +0900

    refactor: 패키지 구조 변경

--- src/test/java/com/example/demo/service/UserServiceTest.java
+++ src/test/java/com/example/demo/service/UserServiceTest.java
@@ -4,12 +4,13 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 
-import com.example.demo.exception.CertificationCodeNotMatchedException;
-import com.example.demo.exception.ResourceNotFoundException;
-import com.example.demo.model.UserStatus;
-import com.example.demo.model.dto.UserCreateDto;
-import com.example.demo.model.dto.UserUpdateDto;
-import com.example.demo.repository.UserEntity;
+import com.example.demo.common.domain.exception.CertificationCodeNotMatchedException;
+import com.example.demo.common.domain.exception.ResourceNotFoundException;
+import com.example.demo.user.domain.UserStatus;
+import com.example.demo.user.domain.UserCreate;
+import com.example.demo.user.domain.UserUpdate;
+import com.example.demo.user.infrastructure.UserEntity;
+import com.example.demo.user.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -84,7 +85,7 @@ public class UserServiceTest {
   @Test
   void userCreateDto_를_이용하여_유저를_생성할_수_있다() {
     // given
-    UserCreateDto userCreateDto = UserCreateDto.builder()
+    UserCreate userCreate = UserCreate.builder()
         .email("kok202@kakao.com")
         .address("Gyeongi")
         .nickname("kok202-k")
@@ -92,7 +93,7 @@ public class UserServiceTest {
     BDDMockito.doNothing().when(mailSender).send(any(SimpleMailMessage.class)); //SimpleMailMessage를 사용하는 send가 호출돼도 아무것도 하지 마라는 의미
 
     // when
-    UserEntity result = userService.create(userCreateDto);
+    UserEntity result = userService.create(userCreate);
 
     // then
     assertThat(result.getId()).isNotNull();
@@ -103,13 +104,13 @@ public class UserServiceTest {
   @Test
   void userUpdateDto_를_이용하여_유저를_수정할_수_있다() {
     // given
-    UserUpdateDto userUpdateDto = UserUpdateDto.builder()
+    UserUpdate userUpdate = UserUpdate.builder()
         .address("Incheon")
         .nickname("kok202-n")
         .build();
 
     // when
-    userService.update(1, userUpdateDto);
+    userService.update(1, userUpdate);
 
     // then
     UserEntity result = userService.getById(1);
