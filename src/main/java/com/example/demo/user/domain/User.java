55cef362dc621a56653be85485b47284de0f5f69
Yeseo Kim <chinkl850@gmail.com>
2024-07-30 13:43:46 +0900

    feat: 테스트 코드 보강 및 Clock, Uuid 처리

--- src/main/java/com/example/demo/user/domain/User.java
+++ src/main/java/com/example/demo/user/domain/User.java
@@ -3,6 +3,9 @@ package com.example.demo.user.domain;
 import com.example.demo.common.domain.exception.CertificationCodeNotMatchedException;
 import java.time.Clock;
 import java.util.UUID;
+
+import com.example.demo.common.service.port.ClockHolder;
+import com.example.demo.common.service.port.UuidHolder;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -27,13 +30,13 @@ public class User {
     this.lastLoginAt = lastLoginAt;
   }
 
-  public static User from(UserCreate userCreate) {
+  public static User from(UserCreate userCreate, UuidHolder uuidHolder) {
     return User.builder()
         .email(userCreate.getEmail())
         .nickname(userCreate.getNickname())
         .address(userCreate.getAddress())
         .status(UserStatus.PENDING)
-        .certificationCode(UUID.randomUUID().toString())
+        .certificationCode(uuidHolder.random()) // 의존성 역전 적용
         .build();
   }
 
@@ -49,7 +52,7 @@ public class User {
         .build();
   }
 
-  public User login() {
+  public User login(ClockHolder clockHolder) {
     return User.builder()
         .id(id)
         .email(email)
@@ -57,7 +60,7 @@ public class User {
         .address(address)
         .certificationCode(certificationCode)
         .status(status)
-        .lastLoginAt(Clock.systemUTC().millis())
+        .lastLoginAt(clockHolder.millis())
         .build();
   }
 
--- src/test/java/com/example/demo/user/domain/UserTest.java
+++ src/test/java/com/example/demo/user/domain/UserTest.java
@@ -1,52 +1,126 @@
 package com.example.demo.user.domain;
 
+import com.example.demo.common.domain.exception.CertificationCodeNotMatchedException;
+import com.example.demo.common.mock.TestClockHolder;
+import com.example.demo.common.mock.TestUuidHolder;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class UserTest {
 
   @Test
   public void UserCreate_객체로_생성할_수_있다() {
     // given
+    UserCreate userCreate = UserCreate.builder()
+            .email("kok202@kakao.com")
+            .nickname("kok202")
+            .address("Pangyo")
+            .build();
 
     // when
+    User user = User.from(userCreate, new TestUuidHolder("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaab"));
 
     // then
+    assertThat(user.getId()).isNull();
+    assertThat(user.getEmail()).isEqualTo("kok202@kakao.com");
+    assertThat(user.getNickname()).isEqualTo("kok202");
+    assertThat(user.getAddress()).isEqualTo("Pangyo");
+    assertThat(user.getStatus()).isEqualTo(UserStatus.PENDING);
+    assertThat(user.getCertificationCode()).isEqualTo("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaab");
   }
 
   @Test
   public void UserUpdate_객체로_데이터를_업데이트_할_수_있다() {
+
     // given
+    User user = User.builder()
+            .id(1L)
+            .email("kok202@kakao.com")
+            .nickname("kok202")
+            .address("Seoul")
+            .status(UserStatus.ACTIVE)
+            .lastLoginAt(100L)
+            .certificationCode("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaab")
+            .build();
+
+    UserUpdate userUpdate = UserUpdate.builder()
+            .nickname("kok202-k")
+            .address("Pangyo")
+            .build();
 
     // when
+    user = user.update(userUpdate);
 
     // then
+    assertThat(user.getId()).isEqualTo(1L);
+    assertThat(user.getEmail()).isEqualTo("kok202@kakao.com");
+    assertThat(user.getNickname()).isEqualTo("kok202-k");
+    assertThat(user.getAddress()).isEqualTo("Pangyo");
+    assertThat(user.getStatus()).isEqualTo(UserStatus.ACTIVE);
+    assertThat(user.getLastLoginAt()).isEqualTo(100L);
+    assertThat(user.getCertificationCode()).isEqualTo("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaab");
   }
 
   @Test
   public void 로그인을_할_수_있고_로그인시_마지막_로그인_시간이_변경된다() {
     // given
+    User user = User.builder()
+            .id(1L)
+            .email("kok202@kakao.com")
+            .nickname("kok202")
+            .address("Seoul")
+            .status(UserStatus.ACTIVE)
+            .lastLoginAt(100L)
+            .certificationCode("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaab")
+            .build();
 
     // when
+    user = user.login(new TestClockHolder(1678530673958L));
 
     // then
+    assertThat(user.getLastLoginAt()).isEqualTo(1678530673958L);
   }
 
   @Test
   public void 유효한_인증_코드로_계정을_활성화_할_수_있다() {
     // given
+    User user = User.builder()
+            .id(1L)
+            .email("kok202@kakao.com")
+            .nickname("kok202")
+            .address("Seoul")
+            .status(UserStatus.PENDING)
+            .lastLoginAt(100L)
+            .certificationCode("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaab")
+            .build();
 
     // when
+    user = user.certification("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaab");
 
     // then
+    assertThat(user.getStatus()).isEqualTo(UserStatus.ACTIVE);
   }
 
   @Test
   public void 잘못된_인증_코드로_계정을_활성화_하려면_에러를_던진다() {
     // given
+    User user = User.builder()
+            .id(1L)
+            .email("kok202@kakao.com")
+            .nickname("kok202")
+            .address("Seoul")
+            .status(UserStatus.PENDING)
+            .lastLoginAt(100L)
+            .certificationCode("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaab")
+            .build();
 
     // when
-
     // then
+    assertThatThrownBy(() -> user.certification("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaac"))
+            .isInstanceOf(CertificationCodeNotMatchedException.class);
+
   }
 
 }
